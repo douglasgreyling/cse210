2046f8b9a5b45a9794275e8331ad12799dbb3af3
Douglas Greyling <greyling.douglas@gmail.com>
2024-10-02 18:25:38 +0200

    crimesjohnson: Finish learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Fraction wholeNumber = new Fraction(6);
+        Fraction sixSevenths = new Fraction(6, 7);
+
+        int top = sixSevenths.GetTop();
+        sixSevenths.SetTop(top + 1);
+
+        int bottom = sixSevenths.GetBottom();
+        sixSevenths.SetBottom(bottom + 1);
+
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        Fraction five = new Fraction(5);
+
+        Console.WriteLine(five.GetFractionString());
+        Console.WriteLine(five.GetDecimalValue());
+
+        Fraction threeQuarters = new Fraction(3, 4);
+
+        Console.WriteLine(threeQuarters.GetFractionString());
+        Console.WriteLine(threeQuarters.GetDecimalValue());
+
+        Fraction oneThird = new Fraction(1, 3);
+
+        Console.WriteLine(oneThird.GetFractionString());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
