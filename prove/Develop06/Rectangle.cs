b228ffe650c88bb6c420eb74d09ce988547e313d
Douglas Greyling <greyling.douglas@gmail.com>
2024-10-23 19:33:32 +0200

    crimesjohnson: Finish develop 6

---
+++ prove/Develop06/Rectangle.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Rectangle : Shape
+{
+    private double _width;
+    private double _length;
+
+    public Rectangle(string color, double width, double length) : base(color)
+    {
+        _width = width;
+        _length = length;
+    }
+
+    public override double GetArea() {
+        return _width * _length;
+    }
+}
